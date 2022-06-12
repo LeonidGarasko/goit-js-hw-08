e13b8ac7b7bd38b55d3d0f9bbc7e5f085efacf98
LeonidGarasko <djjiehuh@gmail.com>
2022-06-12 18:28:16 +0300

    Update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+let data = {
+    email: '',
+    message: ''
+};
+
+const saveMessageObject =  JSON.parse(localStorage.getItem("feedback-form-state"))
+if (saveMessageObject) {
+    data.email = saveMessageObject.email;
+    data.message = saveMessageObject.message;
+}
+
+form.querySelector('[name="email"]').value = data.email;
+form.querySelector('[name="message"]').value = data.message;
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
+    data[e.target.name] = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+
+    localStorage.clear();
+    e.target.reset();
+    data.email = '';
+    data.message = '';
+}
