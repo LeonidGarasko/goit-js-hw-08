8334199ed964e39eaa7ec42ec4ea560e652aa28c
LeonidGarasko <djjiehuh@gmail.com>
2022-06-15 11:54:52 +0300

    wqqe

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,8 +2,13 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+const time = localStorage.getItem("videoplayer-current-time")
+if (!time) {
+    return;
+} else {
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
+}
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
 
 player.on('timeupdate',throttle((data) => {
     localStorage.setItem("videoplayer-current-time", data.seconds);
