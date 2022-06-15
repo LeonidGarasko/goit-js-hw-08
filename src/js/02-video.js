4b579f0567b04df499f4b19dc1514a5dad7c7411
LeonidGarasko <djjiehuh@gmail.com>
2022-06-15 11:48:36 +0300

    qqq

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,10 +3,12 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
+
 player.on('timeupdate',throttle((data) => {
     localStorage.setItem("videoplayer-current-time", data.seconds);
 },1000));
 
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then().catch();
+
 
